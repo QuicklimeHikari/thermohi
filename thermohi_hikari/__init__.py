5e318e94c169d8d714ee495133357b8295ce6642
Hikari <caoyang@Hikari-Chans-ThinkPad.local>
2025-11-06 17:10:20 +0800

    0.1.2, readme.md added.

--- thermohi_hikari/__init__.py
+++ thermohi_hikari/__init__.py
@@ -2,7 +2,7 @@
 from .datalistio import DataList
 from .thermoanalysis import KineticAnalysis, r_square_xy
 from .plotting import FittingPlot
-__version__ = '0.1.3'
+__version__ = '0.1.2'
 __author__ = "Hikari Quicklime"
 __email__ = "897237104@qq.com"
 __all__ = ["DataList", "KineticAnalysis", "r_square_xy","FittingPlot"]
